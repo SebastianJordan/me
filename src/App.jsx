c0af1f186c65eaf978db8a1847778b85f4b71cdf
Sebastian Jordan <sebastianjordan19@gmail.com>
2024-05-12 00:06:11 -0500

    comment furute components

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import styles from './styles/App.module.css'
 import { Navbar } from './components/Navbar'
 import { HeaderMain } from './components/HeaderMain'
-import AboutMe from './components/AboutMe'
-// import { FooterPage } from './components/Footer'
+import  AboutMe  from './components/AboutMe'
+// import Experience from './components/Experience'
+
 function App() {
   return (
     <>
       <div className={styles.page}>
         <Navbar showMenu={true} />
         <HeaderMain />
+        {/* <Experience /> */}
         <AboutMe />
       </div>
-      {/* <FooterPage /> */}
     </>
   )
 }
